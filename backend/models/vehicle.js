294d754419f616a851c13e019ecd17f95b7ffaab
Purvi Soni <purvi@Purvis-MacBook-Air.local>
2025-05-20 01:34:52 +0530

    Update default image component

--- backend/models/vehicle.js
+++ backend/models/vehicle.js
@@ -1,21 +1,17 @@
 const mongoose = require("mongoose");
 
-
 const vehicle_Schema = new mongoose.Schema({
   vehicleName: {
     type: String,
     unique: true,
     required: true,
   },
-  vehicleImage: {              
+  vehicleImage: {
     type: String,
-    default: "default SUV.webp"
-  }
+    default: "default_vehicle.png",
+  },
 });
 
-const vehicleModel = mongoose.model("vehicleModel", vehicle_Schema);  //mapping vehicle_model data into vehicle_data
-
-
+const vehicleModel = mongoose.model("vehicleModel", vehicle_Schema); //mapping vehicle_model data into vehicle_data
 
 module.exports = vehicleModel;
-
